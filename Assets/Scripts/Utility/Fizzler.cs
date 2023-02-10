bfae48a5c96e58582b825e1bc48e52bbfd0fd766
Thomas Burniston <thomas.burniston@gmail.com>
2023-02-10 15:05:32 +0000

    sync

---
+++ Assets/Scripts/Utility/Fizzler.cs
@@ -0,0 +1,74 @@
+﻿
+using UnityEngine;
+
+
+public class Fizzler : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Renderer mainRenderer;
+
+    [Header("Config")]
+    [SerializeField] private float fizzleDuration = 2.5f;
+    [SerializeField] private bool toUpdate;
+
+    private float fizzleStartTime = 0.0f;
+
+    public bool isFizzling { get; private set; } = false;
+    public bool hasFizzled { get; private set; } = false;
+    public float fizzlePct { get; private set; } = 0.0f;
+
+
+    private void Awake()
+    {
+        // Instantiate seperate materials
+        for (int i = 0; i < mainRenderer.materials.Length; i++)
+        {
+            mainRenderer.materials[i] = Instantiate(mainRenderer.materials[i]);
+        }
+    }
+
+
+    private void Update()
+    {
+        if (toUpdate) CallUpdate();
+    }
+
+    public void CallUpdate()
+    {
+        if (!isFizzling) return;
+
+        // Update variables
+        fizzlePct = Mathf.Min((Time.time - fizzleStartTime) / fizzleDuration, 1.0f);
+        foreach (Material mat in mainRenderer.materials) mat.SetFloat("_Percent", fizzlePct);
+        if (fizzlePct >= 1.0f)
+        {
+            hasFizzled = true;
+            isFizzling = false;
+        }
+    }
+
+    [ContextMenu("Start Fizzle")]
+    public void StartFizzle()
+    {
+        if (isFizzling || hasFizzled) return;
+
+        // Start fizzle
+        fizzleStartTime = Time.time;
+        isFizzling = true;
+        hasFizzled = false;
+        fizzlePct = 0.0f;
+        CallUpdate();
+    }
+
+    [ContextMenu("Reset")]
+    public void Reset()
+    {
+        if (!isFizzling && !hasFizzled) return;
+
+        // Reset fizzle variables
+        isFizzling = false;
+        hasFizzled = false;
+        fizzlePct = 0.0f;
+        foreach (Material mat in mainRenderer.materials) mat.SetFloat("_Percent", 0.0f);
+    }
+}
---
+++ Assets/Scripts/Utility/PlaceLerper.cs
@@ -0,0 +1,81 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlaceLerper : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform[] placeList;
+    [SerializeField] private bool log;
+
+    private Dictionary<string, Transform> places = new Dictionary<string, Transform>();
+
+    public float inPositionThreshold = 0.2f;
+    public float isRotatedThreshold = 0.2f;
+    public float positionlerpSpeed = 3.0f;
+    public float rotationLerpSpeed = 3.0f;
+    private bool toLerpPosition;
+    private bool toLerpRotation;
+    public Vector3 targetOffsetPosition { get; private set; } = Vector3.zero;
+    public Quaternion targetOffsetRotation { get; private set; } = Quaternion.identity;
+    public Transform currentPlace { get; private set; }
+    public bool inPosition => (currentPlace.position + targetOffsetPosition - transform.position).magnitude < inPositionThreshold;
+    public bool isRotated => Quaternion.Angle(currentPlace.rotation * targetOffsetRotation, transform.rotation) < isRotatedThreshold;
+    public Vector3 targetPosition => currentPlace.position + targetOffsetPosition;
+    public Quaternion targetRotation => currentPlace.rotation * targetOffsetRotation;
+
+
+    private void Awake()
+    {
+        foreach (Transform place in placeList)
+        {
+            places[place.gameObject.name] = place;
+        }
+    }
+
+
+    public void Lerp(bool set = false)
+    {
+        if (toLerpPosition && !set) transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * positionlerpSpeed);
+        else transform.position = targetPosition;
+        if (toLerpRotation && !set) transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * positionlerpSpeed);
+        else transform.rotation = targetRotation;
+    }
+
+
+    public void SetOffset(Vector3 position, Quaternion rotation, bool setPos = false, bool setRot = false)
+    {
+        SetOffsetPosition(position, setPos);
+        SetOffsetRotation(rotation, setRot);
+    }
+
+    public void SetOffsetPosition(Vector3 position, bool set = false)
+    {
+        targetOffsetPosition = position;
+        toLerpPosition = !set;
+        if (set) transform.position = targetPosition;
+    }
+
+    public void SetOffsetRotation(Quaternion rotation, bool set = false)
+    {
+        targetOffsetRotation = rotation;
+        toLerpRotation = !set;
+        if (set) transform.rotation = targetRotation;
+    }
+
+
+    public void SetPlace(string placeName, bool setPos = false, bool setRot = false)
+    {
+        // Dont set if already or doesnt exist
+        if (currentPlace != null && currentPlace.gameObject.name == placeName && !setPos) return;
+        if (!places.ContainsKey(placeName)) return;
+
+        // Set to place
+        currentPlace = places[placeName];
+        toLerpPosition = !setPos;
+        toLerpRotation = !setRot;
+        if (setPos) transform.position = targetPosition;
+        if (setRot) transform.rotation = targetRotation;
+    }
+}
